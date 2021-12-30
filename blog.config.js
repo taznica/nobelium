1e7251f8bc3a24e614c04e87950766ee32f35283
taznica <tailegog@gmail.com>
2021-12-30 16:04:37 +0900

    update: set number of posts to display from 7 to infinite in blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,6 @@ const BLOG = {
   darkBackground: '#0d1117', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
   sortByDate: true,
   showAbout: true,
   showArchive: true,
