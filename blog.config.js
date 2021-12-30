5a4d8fbe7bbd50e6fa40c41da418162ddcac8558
taznica <tailegog@gmail.com>
2021-12-30 16:05:34 +0900

    clean: add spaces before a line

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-//   socialLink: 'https://twitter.com/craigaryhart',
+  //  socialLink: 'https://twitter.com/craigaryhart',
   seo: {
     keywords: ['Blog', 'taznica'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
